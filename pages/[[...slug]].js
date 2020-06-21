c3a7a5f7f72fa0e37bef36d54fe51a7fd2f7d93e
Shusaku Uesugi <shu@chibicode.com>
2020-06-21 16:53:17 -0700

    Implement getStaticProps and getStaticPaths

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -1,17 +1,28 @@
 import Layout from '../components/layout'
+import { getSlugs, getData } from '../lib/api'
 
-export default function Page() {
+export default function Page({ allPages, page }) {
   return (
-    <Layout
-      title='Test Page'
-      menuItems={[
-        {
-          slug: '',
-          title: 'Test Page'
-        }
-      ]}
-    >
-      <p>Body</p>
+    <Layout title={page.title} menuItems={allPages}>
+      {page.body}
     </Layout>
   )
+}
+
+export async function getStaticPaths() {
+  const paths = await getSlugs()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { allPages, page } = await getData(params.slug[0])
+  return {
+    props: {
+      allPages,
+      page
+    }
+  }
 }
